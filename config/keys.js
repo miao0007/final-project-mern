82e9d6b9b7fed80dd0efaf62a7573452b44ef6a2
miao0007 <miao188@hotmail.com>
2020-10-15 19:31:40 +1030

    create passport.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://miao0007:80598286mhc@cluster0.b5e1w.mongodb.net/charities?retryWrites=true&w=majority"
-}
+    mongoURI: "mongodb+srv://miao0007:80598286mhc@cluster0.b5e1w.mongodb.net/charities?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
