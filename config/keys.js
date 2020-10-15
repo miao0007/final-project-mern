2b9f016b1cd5ceaf5aaddc2e1c0828cf44748451
miao0007 <miao188@hotmail.com>
2020-10-15 20:31:52 +1030

    fixed up password validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://miao0007:80598286mhc@cluster0.b5e1w.mongodb.net/charities?retryWrites=true&w=majority",
-    secretOrKey: "secret"
+  mongoURI: "mongodb+srv://miao0007:80598286mhc@cluster0.b5e1w.mongodb.net/charities?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -7,27 +7,35 @@ const users = require("./routes/api/users");
 
 const app = express();
 
-// BodyParser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 
-// connect to MongoDB
-mongoose.connect(db, 
-    { useNewUrlParser: true}
-    ).then(() => console.log("MongoDb successfully connected"))
-    .catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
-    // Passport middleware
+// Passport middleware
 app.use(passport.initialize());
+
 // Passport config
 require("./config/passport")(passport);
+
 // Routes
 app.use("/api/users", users);
 
-    // setup Heroku port for deploying
-    const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-    app.listen(PORT, () => console.log(`Server running on port ${PORT} !`));
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
