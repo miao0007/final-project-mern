d03592fdb58118371d00c0387bc314695b01d809
miao0007 <miao188@hotmail.com>
2020-10-24 01:58:29 +1030

    deploy

--- config/keys.js
+++ config/keys.js
@@ -9,8 +9,3 @@ module.exports = {
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./keys_prod');
-// } else {
-//   module.exports = require('./keys_dev');
-// }
